c28abcda0439ae90cb034a7263feae322b4dac28
seonghun-snu <h2o0318@naver.com>
2021-12-30 23:45:44 +0900

    add mobile version

--- change_scale.js
+++ change_scale.js
@@ -8,44 +8,59 @@ var background = document.getElementById("back");
 var sentence_list = document.getElementById("sentence_list");
 var article = document.getElementById("art");
 
+var UserAgent = navigator.userAgent;
+var mobile = (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null);
+
+
 function change_scale() {
     var innerWidth = window.innerWidth;
-    if (innerWidth <= "1200") {
+    if(mobile){
         nav.style.display = 'none';
         list_icon.style.display = 'block';
+        second.style.display = 'block';
+        image.style.marginTop = 0;
+        header.style.height = "60vh";
+        first.style.fontSize = "100%";
+        article.style.top = "60vh";
     }
-    else {
-        nav.style.display = 'block';
-        list_icon.style.display = 'none';
-    }
-
-    if (innerWidth <= "1000") {
-        second.style.display = 'none';
-        if (innerWidth <= "550") {
-            image.style.marginTop = '-20vh';
-            header.style.height = "40vh";
-            first.style.fontSize = "200%";
-            article.style.top = "40vh";
-        }
-        else if (innerWidth <= "770") {
-            image.style.marginTop = '-30vh';
-            header.style.height = "50vh";
-            first.style.fontSize = "300%";
-            article.style.top = "50vh";
+    else{
+        if (innerWidth <= "1200") {
+            nav.style.display = 'none';
+            list_icon.style.display = 'block';
         }
         else {
-            image.style.marginTop = '-40vh';
-            header.style.height = "60vh";
-            first.style.fontSize = "400%";
-            article.style.top = "60vh";
+            nav.style.display = 'block';
+            list_icon.style.display = 'none';
         }
-    }
-    else {
-        second.style.display = 'block';
-        image.style.marginTop = '-350px';
-        header.style.height = "90vh";
-        first.style.fontSize = "400%";
-        article.style.top = "90vh";
+
+        if (innerWidth <= "1000") {
+            second.style.display = 'none';
+            if (innerWidth <= "550") {
+                image.style.marginTop = '-20vh';
+                header.style.height = "40vh";
+                first.style.fontSize = "200%";
+                article.style.top = "40vh";
+            }
+            else if (innerWidth <= "770") {
+                image.style.marginTop = '-30vh';
+                header.style.height = "50vh";
+                first.style.fontSize = "300%";
+                article.style.top = "50vh";
+            }
+            else {
+                image.style.marginTop = '-40vh';
+                header.style.height = "60vh";
+                first.style.fontSize = "400%";
+                article.style.top = "60vh";
+            }
+        }
+        else {
+            second.style.display = 'block';
+            image.style.marginTop = '-350px';
+            header.style.height = "90vh";
+            first.style.fontSize = "400%";
+            article.style.top = "90vh";
+        }
     }
 }
 
