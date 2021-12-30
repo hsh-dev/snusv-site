97f5e9d4763073c74d8992dfe00735de34d98f30
seonghun-snu <h2o0318@naver.com>
2021-12-30 23:13:45 +0900

    list, slide effect added

--- change_scale.js
+++ change_scale.js
@@ -6,6 +6,8 @@ var image = document.getElementById("image");
 var header = document.getElementById("head");
 var background = document.getElementById("back");
 var sentence_list = document.getElementById("sentence_list");
+var article = document.getElementById("art");
+
 function change_scale() {
     var innerWidth = window.innerWidth;
     if (innerWidth <= "1200") {
@@ -20,26 +22,30 @@ function change_scale() {
     if (innerWidth <= "1000") {
         second.style.display = 'none';
         if (innerWidth <= "550") {
-            image.style.marginTop = '-20%';
-            header.style.height = "40%";
+            image.style.marginTop = '-20vh';
+            header.style.height = "40vh";
             first.style.fontSize = "200%";
+            article.style.top = "40vh";
         }
         else if (innerWidth <= "770") {
-            image.style.marginTop = '-30%';
-            header.style.height = "50%";
+            image.style.marginTop = '-30vh';
+            header.style.height = "50vh";
             first.style.fontSize = "300%";
+            article.style.top = "50vh";
         }
         else {
-            image.style.marginTop = '-40%';
-            header.style.height = "60%";
+            image.style.marginTop = '-40vh';
+            header.style.height = "60vh";
             first.style.fontSize = "400%";
+            article.style.top = "60vh";
         }
     }
     else {
         second.style.display = 'block';
         image.style.marginTop = '-350px';
-        header.style.height = "90%";
+        header.style.height = "90vh";
         first.style.fontSize = "400%";
+        article.style.top = "90vh";
     }
 }
 
--- show_list.js
+++ show_list.js
@@ -1,18 +1,37 @@
 var list_on = false;
 var list = document.getElementById("list");
+var list_icon = document.getElementById("list_icon");
+var body = document.getElementsByClassName("article");
+
 function showList() {
-    if (list_on == false) {
-        list.style.display = "block";
-        document.getElementsByClassName("top")[0].style.backgroundColor = 'rgb(0,0,0,0.7)';
-        first.style.display = "none";
-        second.style.display = "none";
-        list_on = true;
-    }
-    else {
-        document.getElementsByClassName("top")[0].style.backgroundColor = '';
-        list.style.display = "none";
-        first.style.display = "block";
-        change_scale();
-        list_on = false;
-    }
+    list.classList.add('slidedown');
+    list.style.display = "block";
+    document.getElementsByClassName("top")[0].style.backgroundColor = 'rgb(0,0,0,0.7)';
+    first.style.display = "none";
+    second.style.display = "none";
+    list_on = true;
 }
+
+function closeList(){
+    list.style.display = "none";
+    document.getElementsByClassName("top")[0].style.backgroundColor = '';
+    first.style.display = "block";
+    list_on = false;
+}
+
+document.getElementById('back').addEventListener('click',function(e){
+    if(e.target.id != 'list_content'){
+        if (list_on) {
+            closeList();
+        }
+    }
+    e.stopPropagation();
+});
+document.getElementsByClassName('article')[0].addEventListener('click', function (e) {
+    if (e.target.id != 'list_content') {
+        if (list_on) {
+            closeList();
+        }
+    }
+    e.stopPropagation();
+});
